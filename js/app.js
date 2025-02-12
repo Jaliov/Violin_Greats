b9096348b4e9c4d8c78ea727f59a4555ec10456b
Joel Rudin <jarud55@gmail.com>
2025-02-12 00:06:23 -0500

    partial hover/iframe edits

--- js/app.js
+++ js/app.js
@@ -20,10 +20,3 @@ hiddenElements.forEach((el) => observer.observe(el));
 function off() {
   document.querySelector(".intro").style.display = "none";
 }
-
-function onBtnClickHandle(youtubeaddr) {
-  setTimeout(function () {
-    window.location = youtubeaddr;
-  }, 3000);
-}
-document.querySelector(".intro").textContent = "Hey buddy";
